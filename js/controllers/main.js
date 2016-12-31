7c373a8cc40f2cb8c71338967fdb48f2a068d43a
Patrick Grenning <pgrennin@gmail.com>
2016-12-30 22:03:15 -0600

    google chart implementation

--- js/controllers/main.js
+++ js/controllers/main.js
@@ -763,16 +763,44 @@ materialAdmin
         //     $scope.products.splice(x, 1);
         // } 
 
-
-    
-
-      
-
-
-      
-      
-
-
-
     }) //end controller
+
+    // Google chart controller
+    .controller("googleChartController", function ($scope) {
+        $scope.myChartObject = {};
+        
+        $scope.myChartObject.type = "PieChart";
+        
+        $scope.onions = [
+            {v: "Onions"},
+            {v: 3},
+        ];
+
+        $scope.myChartObject.data = {"cols": [
+            {id: "t", label: "Topping", type: "string"},
+            {id: "s", label: "Slices", type: "number"}
+        ], "rows": [
+            {c: [
+                {v: "Mushrooms"},
+                {v: 3},
+            ]},
+            {c: $scope.onions},
+            {c: [
+                {v: "Olives"},
+                {v: 31}
+            ]},
+            {c: [
+                {v: "Zucchini"},
+                {v: 1},
+            ]},
+            {c: [
+                {v: "Pepperoni"},
+                {v: 2},
+            ]}
+        ]};
+
+        $scope.myChartObject.options = {
+            'title': 'How Much Pizza I Ate Last Night'
+        };
+    });
 
