d65f07b95f87ad1181e89253a9635c1f09f5f3e6
Patrick Grenning <pgrennin@gmail.com>
2017-05-20 15:28:30 -0500

    heroku config

--- gulpfile.js
+++ gulpfile.js
@@ -21,7 +21,8 @@ gulp.task('serve', function() {
   browserSync({
     server: {
       baseDir: './'
-    }
+    },
+    port: process.env.PORT || 5000
   });
 
   gulp.watch(['*.html', 'css/*.css', 'js/*.js', 'views/*.html', 'template/*.html', './*.html'], {cwd: 'app'}, reload);
